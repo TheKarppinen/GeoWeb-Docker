9285dfa0c2468e78dea00603763dd9022acb0f60
Ville Karppinen <ville.karppinen@fmi.fi>
2019-06-26 14:34:32 +0300

    Automatically remove existing geoweb services and containers before (re)starting

--- start-service.sh
+++ start-service.sh
@@ -9,5 +9,34 @@
 # it may otherwise give 0/1 replicas.
 #   docker-compose up
 
-# Start services
+echo "List services"
+docker service ls
+
+VAR=$(docker service ls | grep geoweb | awk '{print $1}')
+if [[ ! -z "$VAR" ]]
+then
+  echo -e "\nRemove existing geoweb services"
+  docker service rm $(echo "$VAR" | xargs)
+fi
+
+
+echo -e "\n\nList containers"
+docker ps -a
+
+VAR=$(docker ps -a | grep -E "(geoweb|adaguc)" | awk '{print $1}')
+if [[ ! -z "$VAR" ]]
+then
+  echo -e "\nStop geoweb containers"
+  docker stop $(echo "$VAR" | xargs)
+fi
+
+VAR=$(docker ps -a | grep -E "(geoweb|adaguc)" | awk '{print $1}')
+if [[ ! -z "$VAR" ]]
+then
+  echo -e "\nRemove existing geoweb containers"
+  docker rm $(echo "$VAR" | xargs)
+fi
+
+
+echo -e "\n\nStart services"
 docker stack deploy -c docker-compose.yml geoweb
