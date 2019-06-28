9e49cc81448be4fdab3f27553cc6e9e6d0129313
Ville Karppinen <ville.karppinen@fmi.fi>
2019-06-28 11:40:31 +0300

    Healthcheck for frontend

---
+++ geoweb-frontend-nginx-docker/docker/healthcheck.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Using grep to check the response line because end may contain hidden CTRL-M character
+VAR=$(curl -m1 -Is "http://localhost:8080" | head -n 1 | grep "HTTP/1.1 200 OK")
+if [[ -z "$VAR" ]]
+then
+  # Exit with error code
+  exit 1
+fi
