72a3ff68055881332427c9d3e1d31fbfedb86b9f
Ville Karppinen <ville.karppinen@fmi.fi>
2019-07-02 10:13:31 +0300

    Healthcheck more common check for curl response

--- geoweb-backend-docker/healthcheck.sh
+++ geoweb-backend-docker/healthcheck.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Using grep to check the response line because end may contain hidden CTRL-M character
-VAR=$(curl -m1 -Is "http://localhost:8080/backend/versioninfo/version" | head -n 1 | grep "HTTP/1.1 200 OK")
+VAR=$(curl -m1 -Is "http://localhost:8080/backend/versioninfo/version" | head -n 1 | grep "HTTP/1.1 200")
 if [[ -z "$VAR" ]]
 then
   # Exit with error code
--- geoweb-frontend-nginx-docker/docker/healthcheck.sh
+++ geoweb-frontend-nginx-docker/docker/healthcheck.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Using grep to check the response line because end may contain hidden CTRL-M character
-VAR=$(curl -m1 -Is "http://localhost:80" | head -n 1 | grep "HTTP/1.1 200 OK")
+VAR=$(curl -m1 -Is "http://localhost:80" | head -n 1 | grep "HTTP/1.1 200")
 if [[ -z "$VAR" ]]
 then
   # Exit with error code
